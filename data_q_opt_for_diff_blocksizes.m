dca8588b03ba96e7d5335cfe30c28c6861b00eef
Maumauling <tizia@physik.hu-berlin.de>
2016-05-24 15:47:51 +0200

    data stored in matlab
    to be done: diagraph blocksize vs q_opt ---> elapsed time minima

---
+++ data_q_opt_for_diff_blocksizes.m
@@ -0,0 +1,39 @@
+
+
+gpu Unrolling optimized w. q = 2048  elapsed 0.001443 sec gpu_sum: 285645 <<<grid 64 block 128>>>
+
+
+gpu Unrolling optimized w. q = 4096  elapsed 0.003630 sec gpu_sum: 285645 <<<grid 128 block 32>>>
+
+
+gpu Unrolling optimized w. q = 2048  elapsed 0.002147 sec gpu_sum: 285645 <<<grid 128 block 64>>>
+
+
+gpu Unrolling optimized w. q = 2048  elapsed 0.001433 sec gpu_sum: 285645 <<<grid 32 block 256>>>
+
+
+gpu Unrolling optimized w. q = 512  elapsed 0.001435 sec gpu_sum: 285645 <<<grid 64 block 512>>>
+gpu Unrolling optimized w. q = 1024  elapsed 0.001437 sec gpu_sum: 285645 <<<grid 32 block 512>>>
+gpu Unrolling optimized w. q = 2048  elapsed 0.001426 sec gpu_sum: 285645 <<<grid 16 block 512>>>
+gpu Unrolling optimized w. q = 4096  elapsed 0.002143 sec gpu_sum: 285645 <<<grid 8 block 512>>>
+gpu Unrolling optimized w. q = 8192  elapsed 0.003640 sec gpu_sum: 285645 <<<grid 4 block 512>>>
+gpu Unrolling optimized w. q = 16384  elapsed 0.006861 sec gpu_sum: 285645 <<<grid 2 block 512>>>
+gpu Unrolling optimized w. q = 32768  elapsed 0.013297 sec gpu_sum: 285645 <<<grid 1 block 512>>>
+
+
+gpu Interleaved elapsed 0.013292 sec gpu_sum: 285645 <<<grid 16384 block 1024>>>
+gpu Unrolling optimized w. q = 2  elapsed 0.007894 sec gpu_sum: 285645 <<<grid 8192 block 1024>>>
+gpu Unrolling optimized w. q = 4  elapsed 0.004486 sec gpu_sum: 285645 <<<grid 4096 block 1024>>>
+gpu Unrolling optimized w. q = 8  elapsed 0.002826 sec gpu_sum: 285645 <<<grid 2048 block 1024>>>
+gpu Unrolling optimized w. q = 16  elapsed 0.002053 sec gpu_sum: 285645 <<<grid 1024 block 1024>>>
+gpu Unrolling optimized w. q = 32  elapsed 0.001799 sec gpu_sum: 285645 <<<grid 512 block 1024>>>
+gpu Unrolling optimized w. q = 64  elapsed 0.001591 sec gpu_sum: 285645 <<<grid 256 block 1024>>>
+gpu Unrolling optimized w. q = 128  elapsed 0.001501 sec gpu_sum: 285645 <<<grid 128 block 1024>>>
+gpu Unrolling optimized w. q = 256  elapsed 0.001468 sec gpu_sum: 285645 <<<grid 64 block 1024>>>
+gpu Unrolling optimized w. q = 512  elapsed 0.001436 sec gpu_sum: 285645 <<<grid 32 block 1024>>>
+gpu Unrolling optimized w. q = 1024  elapsed 0.001423 sec gpu_sum: 285645 <<<grid 16 block 1024>>>
+gpu Unrolling optimized w. q = 2048  elapsed 0.001420 sec gpu_sum: 285645 <<<grid 8 block 1024>>>
+gpu Unrolling optimized w. q = 4096  elapsed 0.002100 sec gpu_sum: 285645 <<<grid 4 block 1024>>>
+gpu Unrolling optimized w. q = 8192  elapsed 0.003720 sec gpu_sum: 285645 <<<grid 2 block 1024>>>
+gpu Unrolling optimized w. q = 16384  elapsed 0.006968 sec gpu_sum: 285645 <<<grid 1 block 1024>>>
+
